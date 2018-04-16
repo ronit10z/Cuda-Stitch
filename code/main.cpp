7ba0567af8288a4634e5c2fefa2a58ad47903cea
Ronit Banerjee <ronitb@latedays.andrew.cmu.edu>
2018-04-16 14:37:34 -0400

    first commit

---
+++ code/main.cpp
@@ -0,0 +1,21 @@
+#include "opencv/cv.h"
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/core.hpp>
+// #include <opencv2/imgcodecs.hpp>
+// #include <opencv2/imgproc.hpp>
+// #include <opencv2/highgui.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits>
+
+
+// using namespace cv;
+
+int main()
+{
+  printf("Hello World\n");
+}
