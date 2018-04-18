e63860c66e10368d9a6b85a4da37c4999007fac9
Ronit Banerjee <ronitb@ghc37.ghc.andrew.cmu.edu>
2018-04-18 10:13:27 -0400

    build system working

--- code/makeTemp.sh
+++ code/makeTemp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ main.cpp -I/afs/cs.cmu.edu/academic/class/15418-s18/public/opencv-418/include -L/afs/cs.cmu.edu/academic/class/15418-s18/public/opencv-418/lib64 -lopencv_core
+g++ -I/afs/cs.cmu.edu/academic/class/15418-s18/public/opencv-418/include main.cpp -L/afs/cs.cmu.edu/academic/class/15418-s18/public/opencv-418/lib64 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_stitching -lopencv_imgproc -o stitch
---
+++ code_ghc/CMakeFiles/CUDA-STITCH.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CUDA-STITCH.dir/main.cpp.o"
+  "CUDA-STITCH.pdb"
+  "CUDA-STITCH"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CUDA-STITCH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
