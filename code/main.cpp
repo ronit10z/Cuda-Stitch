74e28ec4ed3c7add8a4fd08624237dc8db4f79ae
Ronit Banerjee <ronitb@ghc37.ghc.andrew.cmu.edu>
2018-04-22 10:18:14 -0400

    image showing

--- code/main.cpp
+++ code/main.cpp
@@ -1,138 +1,64 @@
-
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/stitching.hpp"
-#include <opencv2/opencv.hpp>
-
-
+#include <ctime>
 #include <iostream>
 
-using namespace std;
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/stitching.hpp>
+#include <opencv2/opencv.hpp>
+
 using namespace cv;
+using namespace std;
 
-bool try_use_gpu = false;
-bool divide_images = false;
-Stitcher::Mode mode = Stitcher::PANORAMA;
-vector<Mat> imgs;
-string result_name = "result.jpg";
+#define PROCEDURE 1
 
-void printUsage(char** argv);
-int parseCmdArgs(int argc, char** argv);
+//-------------------------------------------------------
 
-int main(int argc, char* argv[])
+static inline void GenerateIntegralImage(const Mat &source, Mat &integralImage);
+
+int main(int argc, char const *argv[])
 {
-	int retval = parseCmdArgs(argc, argv);
-	if (retval) return -1;
+  // Declare Ipoints and other stuff
+  Mat img = imread("../../images/bryce_left_02.png");
+  Mat gray8(img.size(), CV_8U, 1);
+  Mat gray32(img.size(), CV_32F, 1);
 
-	Mat pano;
-	Ptr<Stitcher> stitcher = Stitcher::create(mode, try_use_gpu);
-	Stitcher::Status status = stitcher->stitch(imgs, pano);
+  cvtColor(img, gray8, CV_BGR2GRAY);
+  gray8.convertTo(gray32, CV_32F, 1.0/255.0, 0);
 
-	if (status != Stitcher::OK)
-	{
-		cout << "Can't stitch images, error code = " << int(status) << endl;
-		return -1;
-	}
+  Mat integralImage(img.size(), CV_32F, 1);
+  GenerateIntegralImage(gray32, integralImage);
 
-	imwrite(result_name, pano);
-	cout << "stitching completed successfully\n" << result_name << " saved!";
-	imshow(result_name, pano);
-	cvWaitKey(0);
-	return 0;
+
+  imshow("l", img);
+  waitKey(0);
+
+  return 0;
+}
+
+// pass by reference
+static inline void GenerateIntegralImage(const Mat &source, Mat &integralImage)
+{
+
+  integralImage.at<float>(0, 0) = source.at<float>(0, 0);
+  for (int j = 1; j < integralImage.cols; ++j)
+  {
+    integralImage.at<float>(0, j) = integralImage.at<float>(0, j - 1) + 
+    source.at<float>(0, j);
+  }
+
+  for (int i = 1; i < integralImage.rows; ++i)
+  {
+    float accumulator = 0.0f;
+    for (int j = 0; j < integralImage.cols; ++j)
+    {
+      accumulator += source.at<float>(i, j);
+      integralImage.at<float>(i, j) = accumulator + integralImage.at<float>(i - 1, j);
+    }
+  }
 }
 
 
-void printUsage(char** argv)
+inline int fRound(float flt)
 {
-	cout <<
-		 "Images stitcher.\n\n" << "Usage :\n" << argv[0] <<" [Flags] img1 img2 [...imgN]\n\n"
-		 "Flags:\n"
-		 "  --d3\n"
-		 "      internally creates three chunks of each image to increase stitching success\n"
-		 "  --try_use_gpu (yes|no)\n"
-		 "      Try to use GPU. The default value is 'no'. All default values\n"
-		 "      are for CPU mode.\n"
-		 "  --mode (panorama|scans)\n"
-		 "      Determines configuration of stitcher. The default is 'panorama',\n"
-		 "      mode suitable for creating photo panoramas. Option 'scans' is suitable\n"
-		 "      for stitching materials under affine transformation, such as scans.\n"
-		 "  --output <result_img>\n"
-		 "      The default is 'result.jpg'.\n\n"
-		 "Example usage :\n" << argv[0] << " --d3 --try_use_gpu yes --mode scans img1.jpg img2.jpg\n";
-}
-
-
-int parseCmdArgs(int argc, char** argv)
-{
-	if (argc == 1)
-	{
-		printUsage(argv);
-		return -1;
-	}
-
-	for (int i = 1; i < argc; ++i)
-	{
-		if (string(argv[i]) == "--help" || string(argv[i]) == "/?")
-		{
-			printUsage(argv);
-			return -1;
-		}
-		else if (string(argv[i]) == "--try_use_gpu")
-		{
-			if (string(argv[i + 1]) == "no")
-				try_use_gpu = false;
-			else if (string(argv[i + 1]) == "yes")
-				try_use_gpu = true;
-			else
-			{
-				cout << "Bad --try_use_gpu flag value\n";
-				return -1;
-			}
-			i++;
-		}
-		else if (string(argv[i]) == "--d3")
-		{
-			divide_images = true;
-		}
-		else if (string(argv[i]) == "--output")
-		{
-			result_name = argv[i + 1];
-			i++;
-		}
-		else if (string(argv[i]) == "--mode")
-		{
-			if (string(argv[i + 1]) == "panorama")
-				mode = Stitcher::PANORAMA;
-			else if (string(argv[i + 1]) == "scans")
-				mode = Stitcher::SCANS;
-			else
-			{
-				cout << "Bad --mode flag value\n";
-				return -1;
-			}
-			i++;
-		}
-		else
-		{
-			Mat img = imread(argv[i]);
-			if (img.empty())
-			{
-				cout << "Can't read image '" << argv[i] << "'\n";
-				return -1;
-			}
-
-			if (divide_images)
-			{
-				Rect rect(0, 0, img.cols / 2, img.rows);
-				imgs.push_back(img(rect).clone());
-				rect.x = img.cols / 3;
-				imgs.push_back(img(rect).clone());
-				rect.x = img.cols / 2;
-				imgs.push_back(img(rect).clone());
-			}
-			else
-				imgs.push_back(img);
-		}
-	}
-	return 0;
+  return (int) floor(flt+0.5f);
 }
