23a8a4aa98b8b3878879a49dc52f502c2848d9eb
Ronit Banerjee <ronitb@ghc37.ghc.andrew.cmu.edu>
2018-04-22 23:29:32 -0400

    bashrc

--- etc/makeTemp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -I/afs/cs.cmu.edu/academic/class/15418-s18/public/opencv-418/include main.cpp -L/afs/cs.cmu.edu/academic/class/15418-s18/public/opencv-418/lib64 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_stitching -lopencv_imgproc -o stitch
